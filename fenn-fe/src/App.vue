62d4c6c24196f05e8a7bf79d3263c200c171a85b
Ilman Nafian <milmannafian04@gmail.com>
2021-02-02 15:44:47 +0700

    clear vue template

--- fenn-fe/src/App.vue
+++ fenn-fe/src/App.vue
@@ -1,27 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Home />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Home from './components/Home.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld,
+    Home,
   },
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ fenn-fe/src/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <span>lol</span>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
+
+<style scoped></style>
