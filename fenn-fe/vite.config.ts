3778a49258a66e1bbc93070e5004032669f561f0
Ilman Nafian <milmannafian04@gmail.com>
2021-02-05 00:48:39 +0700

    ¯\_(ツ)_/¯

--- fenn-fe/vite.config.ts
+++ fenn-fe/vite.config.ts
@@ -1,12 +1,10 @@
+import reactRefresh from '@vitejs/plugin-react-refresh';
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [reactRefresh()],
   server: {
-    hmr: {
-      host: 'localhost',
-    },
+    hmr: { host: 'localhost' },
   },
 });
