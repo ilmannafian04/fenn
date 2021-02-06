4d88178ff589320e9a1c104dd8dbdefc0fccc811
Ilman Nafian <milmannafian04@gmail.com>
2021-02-06 18:31:23 +0700

    ditch nav drawer since it wont be used that much

--- fenn-fe/src/components/App.tsx
+++ fenn-fe/src/components/App.tsx
@@ -1,28 +1,21 @@
 import { Box, Container, createStyles, makeStyles } from '@material-ui/core';
-import React, { useState } from 'react';
+import React from 'react';
 
-import NavBar from './navs/NavBar';
-import NavDrawer from './navs/NavDrawer';
+import NavBar from './NavBar';
 import Home from './Home';
 
 const useStyles = makeStyles((theme) =>
   createStyles({
     toolbar: theme.mixins.toolbar,
-    content: {
-      flexGrow: 1,
-      padding: theme.spacing(3),
-    },
   })
 );
 
 const App = () => {
-  const [drawerOpen, setDrawerOpen] = useState(false);
   const classes = useStyles();
   return (
     <Box display="flex">
-      <NavBar drawerState={drawerOpen} drawerOpenFn={setDrawerOpen} />
-      <NavDrawer open={drawerOpen} setOpen={setDrawerOpen} />
-      <Container className={classes.content}>
+      <NavBar />
+      <Container>
         <div className={classes.toolbar} />
         <Home />
       </Container>
