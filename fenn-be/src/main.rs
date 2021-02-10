e0fbd46e799032882eef009a2e796b1663fbb064
ilmannafian04 <milmannafian04@gmail.com>
2021-02-10 14:14:06 +0700

    init diesel

--- fenn-be/src/main.rs
+++ fenn-be/src/main.rs
@@ -1,11 +1,15 @@
 use std::env;
 
 use actix_web::{middleware::Logger, App, HttpServer};
+use diesel::PgConnection;
+use diesel::r2d2::{ConnectionManager, Pool};
 use dotenv::dotenv;
 use log::info;
 
 mod controller;
+mod dto;
 mod route;
+mod schema;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -13,9 +17,15 @@ async fn main() -> std::io::Result<()> {
     env_logger::init();
     info!("starting server");
 
-    HttpServer::new(|| {
+    info!("connecting to database");
+    let db_manager = ConnectionManager::<PgConnection>::new(env::var("DATABASE_URL").expect("DATABASE_URL not set"));
+    let db_pool = Pool::builder().build(db_manager).expect("failed to create database pool");
+    info!("server connected to database");
+
+    HttpServer::new(move || {
         App::new()
             .configure(route::configure)
+            .data(db_pool.clone())
             .wrap(Logger::default())
     })
     .bind(format!(
