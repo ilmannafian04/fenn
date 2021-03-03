a5c224a978214791e5c5e82ca51e69150b91b39a
ilmannafian04 <milmannafian04@gmail.com>
2021-03-03 13:52:09 +0700

    create base ui structure

--- fenn-fe/src/components/polls/FillPoll.tsx
+++ fenn-fe/src/components/polls/FillPoll.tsx
@@ -1,18 +1,33 @@
+import { Box, Paper } from '@material-ui/core';
 import axios from 'axios';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+
+import Poll from '../../types/Poll';
 
 const FillPoll = () => {
   const { pollId } = useParams<{ pollId: string }>();
+  const [poll, setPoll] = useState<Poll>();
   useEffect(() => {
     axios
-      .get('/api/poll', { params: { id: pollId } })
+      .get<Poll>('/api/poll', { params: { id: pollId } })
       .then((value) => {
-        console.log(value);
+        setPoll(value.data);
       })
       .catch((err) => console.error(err));
   }, []);
-  return (
+  return poll ? (
+    <>
+      <h1>{poll.title}</h1>
+      <Paper square>
+        <Box padding="1rem">
+          {poll.options.map((value, index) => (
+            <h2 key={index}>{value.name}</h2>
+          ))}
+        </Box>
+      </Paper>
+    </>
+  ) : (
     <>
       <h1>loading</h1>
     </>
---
+++ fenn-fe/src/types/Poll.tsx
@@ -0,0 +1,10 @@
+export interface PollOption {
+  id: number;
+  name: string;
+}
+
+export default interface Poll {
+  id: number;
+  title: string;
+  options: PollOption[];
+}
